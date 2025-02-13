4a2aca68e313a8723d162e7e8bf9b198017d9a1c
Milky-Siraj <milky.siraj@aastustudent.edu.et>
2025-02-13 09:03:19 -0500

    created room_form.html

--- base/views.py
+++ base/views.py
@@ -17,5 +17,8 @@ def  home(request):
 def room(request, pk):
     room=Room.objects.get(id=pk)
     context={'room': room}
-    return render(request,'base/room.html',context)
+    return render(request,'base/room.html', context)
 
+def createRoom(request):
+    context={}
+    return render(request, 'base/room_form.html', context)
